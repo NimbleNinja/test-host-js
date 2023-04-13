d5dd6a808b1930d1ae93efae124c8d65e4e2ae66
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-04-13 11:58:20 +0300

    remove func

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-;(function inject() {
-  document.body.style.backgroundColor = 'blue'
-  const btn = document.createElement('button')
-  btn.textContent = 'change background color'
-  btn.style.fontSize = '20px'
-  document.body.appendChild(btn)
-  btn.addEventListener('click', () => {
-    document.body.style.backgroundColor = 'white'
-    alert('listener work')
-  })
-})()
+document.body.style.backgroundColor = 'blue'
+const btn = document.createElement('button')
+btn.textContent = 'change background color'
+btn.style.fontSize = '20px'
+document.body.appendChild(btn)
+btn.addEventListener('click', () => {
+  document.body.style.backgroundColor = 'white'
+  alert('listener work')
+})
