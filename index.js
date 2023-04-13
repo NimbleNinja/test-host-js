147cdb3dc007446d4bf8e37f5264cf2de6c15c45
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-04-13 11:43:35 +0300

    test event

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-(function inject() {
+;(function inject() {
   document.body.style.backgroundColor = 'blue'
   const title = document.createElement('h1')
   title.textContent = 'WOOOOOOOOOOOOOOOOOORK'
   title.style.color = 'green'
   title.style.fontSize = '50px'
   document.body.appendChild(title)
+  title.addEventListener('click', () => {
+    alert('listener work')
+    document.body.style.backgroundColor = 'white'
+  })
 })()
