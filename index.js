722b2ef89086dcb09f782c2bd4896c34afd080e0
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-04-13 11:54:20 +0300

    add btn

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 ;(function inject() {
   document.body.style.backgroundColor = 'blue'
-  const title = document.createElement('h1')
-  title.textContent = 'WOOOOOOOOOOOOOOOOOORK'
-  title.style.color = 'green'
-  title.style.fontSize = '50px'
-  document.body.appendChild(title)
-  title.addEventListener('click', () => {
-    alert('listener work')
+  const btn = document.createElement('button')
+  btn.textContent = 'change background color'
+  btn.style.fontSize = '20px'
+  document.body.appendChild(btn)
+  btn.addEventListener('click', () => {
     document.body.style.backgroundColor = 'white'
+    alert('listener work')
   })
 })()
