13d0088010f5588d2cb904346d739e495dee7aab
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-04-13 11:27:44 +0300

    add title

--- index.js
+++ index.js
@@ -3,4 +3,7 @@
   setTimeout(() => {
     document.body.style.backgroundColor = 'red'
   }, 3000)
+  const title = document.createElement('h1')
+  title.textContent = 'WOOOOOOOOOOOOOOOOOORK'
+  document.body.appendChild(title)
 })()
