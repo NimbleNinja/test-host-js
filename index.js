3d5b0a78a304d116ea48193b76dc130a6d28588c
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-04-13 13:29:03 +0300

    test css

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-document.body.style.backgroundColor = 'blue'
 const btn = document.createElement('button')
 btn.textContent = 'change background color'
-btn.style.fontSize = '20px'
-document.body.insertBefore(btn, document.querySelector('.FPdoLc'))
+btn.classList.add('btn')
+
+document.body.appendChild(btn)
+
 btn.addEventListener('click', () => {
   document.body.style.backgroundColor = 'white'
   alert('listener work')
