03c6974f4c7036e094f11b866f1339d2ec77a591
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-04-13 12:15:24 +0300

    insertBefore

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.body.style.backgroundColor = 'blue'
 const btn = document.createElement('button')
 btn.textContent = 'change background color'
 btn.style.fontSize = '20px'
-document.body.appendChild(btn)
+document.body.insertBefore(btn, document.querySelector('.FPdoLc'))
 btn.addEventListener('click', () => {
   document.body.style.backgroundColor = 'white'
   alert('listener work')
