8fa03a15e9d80e390aa27661d6866d0696e6936d
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-04-25 10:47:36 +0300

    test script: get iaai auth token

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const bidInfoElement = document.querySelector('#vdActionInfo')
-const parentBidInfoElement = bidInfoElement.parentNode
+//const bidInfoElement = document.querySelector('#vdActionInfo')
+//const parentBidInfoElement = bidInfoElement.parentNode
 
-const div = document.createElement('div')
-div.id = 'r_o_o_t'
-parentBidInfoElement.insertBefore(div, bidInfoElement)
+//const div = document.createElement('div')
+//div.id = 'r_o_o_t'
+//parentBidInfoElement.insertBefore(div, bidInfoElement)
+
+;(async function () {
+  const pageData = await fetch('https://login.iaai.com/Identity/Account/Login')
+    .then(resp => resp.text())
+    .catch(err => '')
+  const match = pageData.match(/name="__RequestVerificationToken".*?value="(?<token>[^"]+)"/s)
+
+  ReactNativeWebView.postMessage(match.toString())
+})()
