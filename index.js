64a33aa28fbde784f183c1fbfd5bbc33fb49f778
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-04-25 15:00:08 +0300

    test script: get iaai auth token v5

--- index.js
+++ index.js
@@ -5,18 +5,17 @@
 //div.id = 'r_o_o_t'
 //parentBidInfoElement.insertBefore(div, bidInfoElement)
 
-//;(async function testToken() {
-//  const pageData = await fetch('https://login.iaai.com/Identity/Account/Login')
-//    .then(resp => resp.text())
-//    .catch(err => '')
-//  const match = pageData.match(/name="__RequestVerificationToken".*?value="(?<token>[^"]+)"/s)
-
-//  window.ReactNativeWebView.postMessage('message from webview')
-//  window.ReactNativeWebView.postMessage(match?.groups?.token)
-//})()
 ;(async function testToken() {
-  const res = await fetch('https://api.github.com/users')
-  const data = await res.json()
-
-  window.ReactNativeWebView.postMessage(JSON.stringify(data))
+  const pageData = await fetch('https://login.iaai.com/Identity/Account/Login')
+  const data = await pageData.text()
+  window.ReactNativeWebView.postMessage(data)
+  //const match = data.match(/name="__RequestVerificationToken".*?value="(?<token>[^"]+)"/s)
+  //window.ReactNativeWebView.postMessage(match?.groups?.token)
 })()
+
+//;(async function testToken() {
+//  const res = await fetch('https://api.github.com/users')
+//  const data = await res.json()
+
+//  window.ReactNativeWebView.postMessage(JSON.stringify(data))
+//})()
