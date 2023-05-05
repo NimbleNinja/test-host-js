4377174df8ef056cb1818a3bf11d463b7d93c8ff
Oleg Zaporozhets <zaporozhets93@gmail.com>
2023-05-05 14:06:01 +0300

    add prebid styles

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-//const bidInfoElement = document.querySelector('#vdActionInfo')
-//const parentBidInfoElement = bidInfoElement.parentNode
+//const targetDOM = document.body
 
-//const div = document.createElement('div')
-//div.id = 'r_o_o_t'
-//parentBidInfoElement.insertBefore(div, bidInfoElement)
+//const mutationCallback = mutations => {
+//  for (const mutation of mutations) {
+//    const formElement = mutation.target.querySelector('form[name="prelimBidForm"]')
+//    console.log(formElement)
+//  }
+//}
+//const mutationConfig = { attributes: true, childList: true, subtree: true }
+//const observer = new MutationObserver(mutationCallback)
 
-//;(async function testToken() {
-//  fetch('https://login.iaai.com/Identity/Account/Login')
-//    .then(res => {
-//      window.ReactNativeWebView.postMessage(JSON.stringify(res))
-//    })
-//    .catch(err => {
-//      window.ReactNativeWebView.postMessage(JSON.stringify(err))
-//    })
-//})()
+//observer.observe(targetDOM, mutationConfig)
 
-//window.addEventListener('DOMContentLoaded', () => {
-//  const iframeElement = document.querySelector('#frame-root')
-//  const iframeDocument = iframeElement.contentDocument
-//  iframeDocument.eval(`console.log(window)`)
-//  //const iframeBody = iframeDocument.querySelector('#mainBody')
-//  //console.log(iframeBody)
+//console.log('scr 2')
 
-//  //const div = document.createElement('div')
-//  //div.style = 'width: 100px; height: 100px: background: red'
-//  //iframeBody.prepend(div)
-//})
+const mutationCallback = mutations => {
+  for (const mutation of mutations) {
+    const formElement = mutation.target.querySelector('form[name="prelimBidForm"]')
+
+    if (formElement) {
+      console.log(formElement)
+      observer.disconnect()
+
+      const buttonEll = document.createElement('button')
+      buttonEll.id = 'btn_el'
+      buttonEll.style.backgroundColor = 'red'
+      buttonEll.textContent = 'button'
+
+      buttonEll.addEventListener('click', () => console.log('work'))
+
+      formElement.appendChild(buttonEll)
+      break
+    }
+  }
+}
